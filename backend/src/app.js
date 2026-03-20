63ae786a3a6805f56338378c8a3fa2bf9341b1a3
marco <marcoalejandro.diazcastaneda@gmail.com>
2026-03-19 21:17:18 -0500

    course origin problem solved

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,23 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use("/characters", charactersRoutes);
 
