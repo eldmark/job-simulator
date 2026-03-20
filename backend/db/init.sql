d9dcf8988d4b3614c52fcd8d9c17d1ebc011d48e
marco <marcoalejandro.diazcastaneda@gmail.com>
2026-03-19 21:31:22 -0500

    added seed.js comand to add the characters to the db

--- backend/db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS characters (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    crew TEXT NOT NULL,
-    devil_fruit TEXT NOT NULL,
-    bounty INTEGER NOT NULL,
-    height REAL NOT NULL,
-    is_alive BOOLEAN NOT NULL
-);
